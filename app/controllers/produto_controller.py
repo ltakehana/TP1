c6e59a6e914386a63c165978c6ba887f015fa4d8
Leonardo Takehana <leonardotakehana@gmail.com>
2023-12-02 17:07:37 -0300

    feat/Create ValorInvalidoException for product

--- app/controllers/produto_controller.py
+++ app/controllers/produto_controller.py
@@ -2,7 +2,7 @@
 from app.models.produto_model import ProdutoModel
 from app.schemas.produto_schema import ProdutoSchema
 from app.views.produto_view import ProdutoView
-from app.exceptions import DescricaoEmBrancoException
+from app.exceptions import DescricaoEmBrancoException,ValorInvalidoException
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
 
@@ -13,6 +13,8 @@ class ProdutoController:
 
     def create_produto(self, db: Session, produto: ProdutoSchema):
         try:
+            if produto.custo <=0 or produto.preco_venda <=0 or produto.quantidade_disponivel<=0:
+                raise ValorInvalidoException()
             if not produto.descricao or produto.descricao == "" or not produto.codigo_barras or produto.codigo_barras == "" or not produto.custo or not produto.preco_venda or not produto.quantidade_disponivel:
                 raise DescricaoEmBrancoException()
             
@@ -22,4 +24,6 @@ class ProdutoController:
             db.refresh(produto_db)
             return self.produto_view.format_response(produto_db)
         except DescricaoEmBrancoException as exc:
+            raise HTTPException(status_code=400, detail=str(exc))
+        except ValorInvalidoException as exc:
             raise HTTPException(status_code=400, detail=str(exc))
