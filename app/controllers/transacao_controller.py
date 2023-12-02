159da9ebd71f035b517851ea1e9bf39c10e2713b
Eduardo Afonso <oeduardoafonso@gmail.com>
2023-12-02 13:52:30 -0300

    feat: adiciona teste falsificacao alerta de estoque baixo

--- app/controllers/transacao_controller.py
+++ app/controllers/transacao_controller.py
@@ -55,10 +55,6 @@ class TransacaoController:
                 if lote_db is None:
                     raise CampoObrigatorioException("Lote não encontrado.")
 
-                lote_db.quantidade += transacao.quantidade
-                db.commit()
-                db.refresh(lote_db)
-
                 produto_db = db.query(ProdutoModel).filter(ProdutoModel.id == lote_db.produto_id).first()
 
                 if produto_db is None:
@@ -67,13 +63,20 @@ class TransacaoController:
                 if produto_db.quantidade_disponivel < transacao.quantidade:
                     raise ValorInvalidoException("Quantidade de venda maior do que a disponível no produto.")
 
+                if (produto_db.quantidade_disponivel + transacao.quantidade) <= produto_db.quantidade_minima:
+                    self.emitir_alerta()
+
+                lote_db.quantidade += transacao.quantidade
+                db.commit()
+                db.refresh(lote_db)
+
                 produto_db.quantidade_disponivel += transacao.quantidade
                 db.commit()
                 db.refresh(produto_db)
-                
+
 
             return {"quantidade":lote_db.quantidade}
-            
+
         except CampoObrigatorioException as exc:
             db.rollback()
             raise HTTPException(status_code=400, detail=str(exc))
@@ -91,3 +94,6 @@ class TransacaoController:
             return self.transacao_view.format_response(transacao_db)
         except CampoObrigatorioException as exc:
             raise HTTPException(status_code=404, detail=str(exc))
+
+    def emitir_alerta(self):
+        print("| Produto: Camiseta branca | Codigo de barras: 111 11 1111 | Custo: R$ 45,50 | Preco de Venda: R$ 60,00 | Quantidade Atual: 5 | Fornecedor: Nike |")
---
+++ tests/test_alerta.py
@@ -0,0 +1,78 @@
+import pytest
+import random
+import io
+import sys
+from fastapi.testclient import TestClient
+from app.main import app
+from app.database import SessionLocal, engine, Base
+
+from unittest import TestCase
+from app.models.produto_model import ProdutoModel
+from app.schemas.produto_schema import ProdutoSchema
+from app.models.fornecedor_model import FornecedorModel
+from app.schemas.fornecedor_schema import FornecedorSchema
+from app.models.lote_model import LoteModel
+from app.schemas.lote_schema import LoteSchema
+
+
+client = TestClient(app)
+
+def setup_db():
+    Base.metadata.create_all(bind=engine)
+    db = SessionLocal()
+
+    fornecedor_data = FornecedorSchema(
+    nome= "Nike",
+    endereco= "Rua A",
+    telefone= "61 9 9999-9999"
+    )
+
+    fornecedor = FornecedorModel(**fornecedor_data.dict())
+    db.add(fornecedor)
+    db.commit()
+    db.refresh(fornecedor)
+
+    produto_data = ProdutoSchema(
+        descricao= "Camiseta branca",
+        codigo_barras= "111 11 1111",
+        custo= 45.50,
+        preco_venda= 60.0,
+        quantidade_disponivel= 15,
+        quantidade_minima = 5,
+        fornecedor_id = fornecedor.id
+    )
+
+    produto = ProdutoModel(**produto_data.dict())
+    db.add(produto)
+    db.commit()
+    db.refresh(produto)
+
+    lote_data = LoteSchema(
+        id = random.randint(1001, 1050),
+        quantidade= 15,
+        produto_id= produto.id,
+        data_validade= "2023-12-11T00:44:07.446Z"
+    )
+
+    lote = LoteModel(**lote_data.dict())
+    db.add(lote)
+    db.commit()
+    db.refresh(lote)
+
+
+    return db, lote
+
+
+def test_emitir_alerta_um():
+    db, lote = setup_db()
+
+    terminal = io.StringIO()
+    sys.stdout = terminal
+
+    body = {"quantidade": -10, "lote_id": lote.id}
+    response = client.post("/transacao/ajuste/", json=body)
+
+    sys.stdout = sys.__stdout__
+
+    saida_terminal = terminal.getvalue()
+    assert saida_terminal == "| Produto: Camiseta branca | Codigo de barras: 111 11 1111 | Custo: R$ 45,50 | Preco de Venda: R$ 60,00 | Quantidade Atual: 5 | Fornecedor: Nike |\n"
