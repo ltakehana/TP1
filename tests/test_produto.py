af6a304e830640593c3db8e5c98d179b7107e567
Leonardo Takehana <leonardotakehana@gmail.com>
2023-12-03 20:11:32 -0300

    feat/Falsificacao consulta produto

--- tests/test_produto.py
+++ tests/test_produto.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from app.main import app
 from app.database import SessionLocal, engine, Base
+from app.views.produto_view import ProdutoView
 
 client = TestClient(app)
 
@@ -63,3 +64,28 @@ def test_cadastrar_produto_com_valores_invalidos():
 
 
     db.close()
+
+
+def test_consultar_produto():
+    db = setup_db()
+    
+    produto = ProdutoSchema(
+        codigo_barras= "111 000 010 11",
+        custo= 50.00,
+        descricao="Mais um Teste",
+        preco_venda= 80.00,
+        quantidade_disponivel= 10
+    )
+
+    produto_db = ProdutoModel(**produto.dict())
+    db.add(produto_db)
+    db.commit()
+    db.refresh(produto_db)
+
+    response = client.get('produto/' + produto_db.codigo_barras + '/')
+    
+    assert response.status_code == 200
+    assert response.json() == ProdutoView(produto)
+    
+    db.close()
+    
