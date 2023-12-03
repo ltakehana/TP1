f5ace074738443e28366b0fae3d3a129cdfc4ecc
brenno-silva <brenno.silva037@gmail.com>
2023-12-03 14:05:06 -0300

    feat: Triangulação - Gestão de Transações

--- app/controllers/transacao_controller.py
+++ app/controllers/transacao_controller.py
@@ -1,4 +1,6 @@
 
+from fastapi import HTTPException
+from app.exceptions import ValorInvalidoException
 from app.models.produto_model import ProdutoModel
 from app.models.transacao_model import TransacaoModel, TransacaoRecebimentoModel, TransacaoTransferenciaModel
 from app.schemas.transacao_schema import TransacaoRecebimentoSchema, TransacaoSchema, TransacaoTransferenciaSchema
@@ -9,71 +11,94 @@ from sqlalchemy.orm import Session
 class TransacaoController:
     def __init__(self):
         self.transacao_view = ProdutoView()
+        self.errorMsg = "Quantidade da transação não deve ser menor que zero."
 
     def recebimento_produto(self, db: Session, transacao: TransacaoRecebimentoSchema):
-        produto_db = db.query(ProdutoModel).filter(
-            ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_id).first()
-        produto_db.quantidade_disponivel += transacao.quantidade
+        try:
+            if transacao.quantidade < 0:
+                raise ValorInvalidoException(
+                    message=self.errorMsg)
 
-        transacao_db = TransacaoRecebimentoModel(quantidade=transacao.quantidade, produto_id=produto_db.id,
-                                                 fornecedor_id=transacao.fornecedor_id, filial_id=transacao.filial_id)
-
-        db.add(transacao_db)
-        db.add(produto_db)
-        db.commit()
-        db.refresh(produto_db)
-
-        return self.transacao_view.format_response(produto_db)
-
-    def transacao_produto(self, db: Session, transacao: TransacaoSchema):
-        if transacao.tipo == "ajustes":
-            produto_db = db.query(ProdutoModel).filter(
-                ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_id).first()
-            produto_db.quantidade_disponivel = transacao.quantidade
-        elif transacao.tipo == "devolucao":
             produto_db = db.query(ProdutoModel).filter(
                 ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_id).first()
             produto_db.quantidade_disponivel += transacao.quantidade
-        else:
-            produto_db = db.query(ProdutoModel).filter(
-                ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_id).first()
-            produto_db.quantidade_disponivel -= transacao.quantidade
 
-        transacao_db = TransacaoModel(quantidade=transacao.quantidade, produto_id=produto_db.id,
-                                      tipo=transacao.tipo, filial_id=transacao.filial_id)
+            transacao_db = TransacaoRecebimentoModel(quantidade=transacao.quantidade, produto_id=produto_db.id,
+                                                     fornecedor_id=transacao.fornecedor_id, filial_id=transacao.filial_id)
 
-        db.add(transacao_db)
-        db.add(produto_db)
-        db.commit()
-        db.refresh(produto_db)
+            db.add(transacao_db)
+            db.add(produto_db)
+            db.commit()
+            db.refresh(produto_db)
 
-        return self.transacao_view.format_response(produto_db)
+            return self.transacao_view.format_response(produto_db)
+        except ValorInvalidoException as exc:
+            raise HTTPException(status_code=400, detail=str(exc))
+
+    def transacao_produto(self, db: Session, transacao: TransacaoSchema):
+        try:
+            if transacao.tipo == "ajustes":
+                produto_db = db.query(ProdutoModel).filter(
+                    ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_id).first()
+                produto_db.quantidade_disponivel = transacao.quantidade
+            elif transacao.tipo == "devolucao":
+                if transacao.quantidade < 0:
+                    raise ValorInvalidoException(
+                        message=self.errorMsg)
+                produto_db = db.query(ProdutoModel).filter(
+                    ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_id).first()
+                produto_db.quantidade_disponivel += transacao.quantidade
+            else:
+                if transacao.quantidade < 0:
+                    raise ValorInvalidoException(
+                        message=self.errorMsg)
+                produto_db = db.query(ProdutoModel).filter(
+                    ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_id).first()
+                produto_db.quantidade_disponivel -= transacao.quantidade
+
+            transacao_db = TransacaoModel(quantidade=transacao.quantidade, produto_id=produto_db.id,
+                                          tipo=transacao.tipo, filial_id=transacao.filial_id)
+
+            db.add(transacao_db)
+            db.add(produto_db)
+            db.commit()
+            db.refresh(produto_db)
+
+            return self.transacao_view.format_response(produto_db)
+        except ValorInvalidoException as exc:
+            raise HTTPException(status_code=400, detail=str(exc))
 
     def transferencia_produto(self, db: Session, transacao: TransacaoTransferenciaSchema):
-        lista = []
+        try:
+            if transacao.quantidade < 0:
+                raise ValorInvalidoException(
+                    message=self.errorMsg)
 
-        produtoA = db.query(ProdutoModel).filter(
-            ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_origem).first()
-        produtoB = db.query(ProdutoModel).filter(
-            ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_destino).first()
+            produtoA = db.query(ProdutoModel).filter(
+                ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_origem).first()
+            produtoB = db.query(ProdutoModel).filter(
+                ProdutoModel.codigo_barras == transacao.codigo_barras and ProdutoModel.filial_id == transacao.filial_destino).first()
 
-        produtoA.quantidade_disponivel -= transacao.quantidade
-        produtoB.quantidade_disponivel += transacao.quantidade
+            produtoA.quantidade_disponivel -= transacao.quantidade
+            produtoB.quantidade_disponivel += transacao.quantidade
 
-        transacao_db = TransacaoTransferenciaModel(quantidade=transacao.quantidade, produto_origem=produtoA.id,
-                                                   produto_destino=produtoB.id, filial_origem=transacao.filial_origem, filial_destino=transacao.filial_destino)
+            transacao_db = TransacaoTransferenciaModel(quantidade=transacao.quantidade, produto_origem=produtoA.id,
+                                                       produto_destino=produtoB.id, filial_origem=transacao.filial_origem, filial_destino=transacao.filial_destino)
 
-        db.add(transacao_db)
-        db.add(produtoA)
-        db.add(produtoB)
-        db.commit()
-        db.refresh(produtoA)
-        db.refresh(produtoB)
+            db.add(transacao_db)
+            db.add(produtoA)
+            db.add(produtoB)
+            db.commit()
+            db.refresh(produtoA)
+            db.refresh(produtoB)
 
-        lista.append(produtoA)
-        lista.append(produtoB)
+            lista = []
+            lista.append(produtoA)
+            lista.append(produtoB)
 
-        lista_produtos_view = [self.transacao_view.format_response(
-            produto_model) for produto_model in lista]
+            lista_produtos_view = [self.transacao_view.format_response(
+                produto_model) for produto_model in lista]
 
-        return lista_produtos_view
+            return lista_produtos_view
+        except ValorInvalidoException as exc:
+            raise HTTPException(status_code=400, detail=str(exc))
--- tests/test_transacoes.py
+++ tests/test_transacoes.py
@@ -1,4 +1,4 @@
-import json
+from unittest import TestCase
 from app.models.filial_model import FilialModel
 from app.models.fornecedor_model import FornecedorModel
 from app.models.produto_model import ProdutoModel
@@ -6,154 +6,237 @@ from app.views.produto_view import ProdutoView
 from fastapi.testclient import TestClient
 from app.main import app
 from app.database import SessionLocal, engine, Base
+from parameterized import parameterized
 
 client = TestClient(app)
 
 
-def setup_db():
-    Base.metadata.create_all(bind=engine)
-    db = SessionLocal()
+class TestTransacao(TestCase):
+    def setUp(self):
+        Base.metadata.create_all(bind=engine)
+        self.db = SessionLocal()
 
-    fornecedor = FornecedorModel(nome="Nome do Fornecedor")
-    filialA = FilialModel(nome="Filial", endereco="Rua A",
-                          telefone="(11)1111-1111")
-    filialB = FilialModel(nome="FilialB", endereco="Rua B",
-                          telefone="(22)2222-2222")
-    produtoA = ProdutoModel(descricao="Produto Teste", codigo_barras="QWERTY",
-                            custo=1.0, preco_venda=5.0, quantidade_disponivel=10, filial_id=1)
-    produtoB = ProdutoModel(descricao="Produto Teste", codigo_barras="QWERTY",
-                            custo=1.0, preco_venda=5.0, quantidade_disponivel=10, filial_id=2)
+        fornecedor = FornecedorModel(nome="Nome do Fornecedor")
+        filialA = FilialModel(nome="Filial", endereco="Rua A",
+                              telefone="(11)1111-1111")
+        filialB = FilialModel(nome="FilialB", endereco="Rua B",
+                              telefone="(22)2222-2222")
+        produtoA = ProdutoModel(descricao="Produto Teste", codigo_barras="QWERTY",
+                                custo=1.0, preco_venda=5.0, quantidade_disponivel=10, filial_id=1)
+        produtoB = ProdutoModel(descricao="Produto Teste", codigo_barras="QWERTY",
+                                custo=1.0, preco_venda=5.0, quantidade_disponivel=10, filial_id=2)
 
-    db.add(fornecedor)
-    db.add(filialA)
-    db.add(filialB)
-    db.add(produtoA)
-    db.add(produtoB)
+        self.db.add(fornecedor)
+        self.db.add(filialA)
+        self.db.add(filialB)
+        self.db.add(produtoA)
+        self.db.add(produtoB)
 
-    db.commit()
+        self.db.commit()
 
-    return db
+    @parameterized.expand([
+        (5, "QWERTY", 1, 1),
+        (10, "QWERTY", 1, 1),
+        (15, "QWERTY", 1, 2),
+    ])
+    def test_recebimento(self, quantidade, codigo_barras, fornecedor_id, filial_id):
+        transacao_view = ProdutoView()
 
+        transacao_data = {
+            "quantidade": quantidade,
+            "codigo_barras": codigo_barras,
+            "fornecedor_id": fornecedor_id,
+            "filial_id": filial_id
+        }
 
-def test_recebimento():
-    db = setup_db()
-    transacao_view = ProdutoView()
+        produto = self.db.query(ProdutoModel).filter(
+            ProdutoModel.codigo_barras == codigo_barras and ProdutoModel.filial_id == filial_id).first()
+        produto.quantidade_disponivel += quantidade
 
-    transacao_data = {
-        "quantidade": 5,
-        "codigo_barras": "QWERTY",
-        "fornecedor_id": 1,
-        "filial_id": 1
-    }
+        response = client.post("/transacao/recebimento", json=transacao_data)
 
-    produto = db.query(ProdutoModel).filter(
-        ProdutoModel.codigo_barras == "QWERTY" and ProdutoModel.filial_id == 1).first()
-    produto.quantidade_disponivel += 5
+        assert response.status_code == 200
+        assert response.json() == transacao_view.format_response(produto)
 
-    response = client.post("/transacao/recebimento", json=transacao_data)
+        self.db.close()
 
-    assert response.status_code == 200
-    assert response.json() == transacao_view.format_response(produto)
+    @parameterized.expand([
+        (5, "QWERTY", 1),
+        (10, "QWERTY", 1),
+        (15, "QWERTY", 2),
+    ])
+    def test_ajuste(self, quantidade, codigo_barras, filial_id):
+        transacao_view = ProdutoView()
 
-    db.close()
+        transacao_data = {
+            "quantidade": quantidade,
+            "codigo_barras": codigo_barras,
+            "tipo": "ajustes",
+            "filial_id": filial_id
+        }
 
+        produto = self.db.query(ProdutoModel).filter(
+            ProdutoModel.codigo_barras == codigo_barras and ProdutoModel.filial_id == filial_id).first()
+        produto.quantidade_disponivel = quantidade
 
-def test_ajuste():
-    db = setup_db()
-    transacao_view = ProdutoView()
+        response = client.post("/transacao", json=transacao_data)
 
-    transacao_data = {
-        "quantidade": 5,
-        "codigo_barras": "QWERTY",
-        "tipo": "ajustes",
-        "filial_id": 1
-    }
+        assert response.status_code == 200
+        assert response.json() == transacao_view.format_response(produto)
 
-    produto = db.query(ProdutoModel).filter(
-        ProdutoModel.codigo_barras == "QWERTY" and ProdutoModel.filial_id == 1).first()
-    produto.quantidade_disponivel = 5
+        self.db.close()
 
-    response = client.post("/transacao", json=transacao_data)
+    @parameterized.expand([
+        (5, "QWERTY", 1),
+        (10, "QWERTY", 1),
+        (15, "QWERTY", 2),
+    ])
+    def test_devolucao(self, quantidade, codigo_barras, filial_id):
+        transacao_view = ProdutoView()
 
-    assert response.status_code == 200
-    assert response.json() == transacao_view.format_response(produto)
+        transacao_data = {
+            "quantidade": quantidade,
+            "codigo_barras": codigo_barras,
+            "tipo": "devolucao",
+            "filial_id": filial_id
+        }
 
-    db.close()
+        produto = self.db.query(ProdutoModel).filter(
+            ProdutoModel.codigo_barras == codigo_barras and ProdutoModel.filial_id == filial_id).first()
+        produto.quantidade_disponivel += quantidade
 
+        response = client.post("/transacao", json=transacao_data)
 
-def test_devolucao():
-    db = setup_db()
-    transacao_view = ProdutoView()
+        assert response.status_code == 200
+        assert response.json() == transacao_view.format_response(produto)
 
-    transacao_data = {
-        "quantidade": 5,
-        "codigo_barras": "QWERTY",
-        "tipo": "devolucao",
-        "filial_id": 1
-    }
+        self.db.close()
 
-    produto = db.query(ProdutoModel).filter(
-        ProdutoModel.codigo_barras == "QWERTY" and ProdutoModel.filial_id == 1).first()
-    produto.quantidade_disponivel += 5
+    @parameterized.expand([
+        (5, "QWERTY", 1),
+        (10, "QWERTY", 1),
+        (15, "QWERTY", 2),
+    ])
+    def test_venda(self, quantidade, codigo_barras, filial_id):
+        transacao_view = ProdutoView()
 
-    response = client.post("/transacao", json=transacao_data)
+        transacao_data = {
+            "quantidade": quantidade,
+            "codigo_barras": codigo_barras,
+            "tipo": "venda",
+            "filial_id": filial_id
+        }
 
-    assert response.status_code == 200
-    assert response.json() == transacao_view.format_response(produto)
+        produto = self.db.query(ProdutoModel).filter(
+            ProdutoModel.codigo_barras == codigo_barras and ProdutoModel.filial_id == filial_id).first()
+        produto.quantidade_disponivel -= quantidade
 
-    db.close()
+        response = client.post("/transacao", json=transacao_data)
 
+        assert response.status_code == 200
+        assert response.json() == transacao_view.format_response(produto)
 
-def test_venda():
-    db = setup_db()
-    transacao_view = ProdutoView()
+        self.db.close()
 
-    transacao_data = {
-        "quantidade": 5,
-        "codigo_barras": "QWERTY",
-        "tipo": "venda",
-        "filial_id": 1
-    }
+    @parameterized.expand([
+        (5, "QWERTY", 1, 2),
+        (10, "QWERTY", 1, 2),
+        (15, "QWERTY", 2, 1),
+    ])
+    def test_transferencia(self, quantidade, codigo_barras, filial_origem, filial_destino):
+        transacao_view = ProdutoView()
 
-    produto = db.query(ProdutoModel).filter(
-        ProdutoModel.codigo_barras == "QWERTY" and ProdutoModel.filial_id == 1).first()
-    produto.quantidade_disponivel -= 5
+        transacao_data = {
+            "quantidade": quantidade,
+            "codigo_barras": codigo_barras,
+            "filial_origem": filial_origem,
+            "filial_destino": filial_destino
+        }
 
-    response = client.post("/transacao", json=transacao_data)
+        produto_origem = self.db.query(ProdutoModel).filter(
+            ProdutoModel.codigo_barras == codigo_barras and ProdutoModel.filial_id == filial_origem).first()
+        produto_destino = self.db.query(ProdutoModel).filter(
+            ProdutoModel.codigo_barras == codigo_barras and ProdutoModel.filial_id == filial_destino).first()
 
-    assert response.status_code == 200
-    assert response.json() == transacao_view.format_response(produto)
+        produto_origem.quantidade_disponivel -= quantidade
+        produto_destino.quantidade_disponivel += quantidade
 
-    db.close()
+        response = client.post("/transacao/transferencia", json=transacao_data)
 
+        produtos = []
+        produtos.append(produto_origem)
+        produtos.append(produto_destino)
 
-def test_transferencia():
-    db = setup_db()
-    transacao_view = ProdutoView()
+        assert response.status_code == 200
+        assert response.json() == [transacao_view.format_response(
+            produto_model) for produto_model in produtos]
 
-    transacao_data = {
-        "quantidade": 5,
-        "codigo_barras": "QWERTY",
-        "filial_origem": 1,
-        "filial_destino": 2
-    }
+        self.db.close()
 
-    produto_origem = db.query(ProdutoModel).filter(
-        ProdutoModel.codigo_barras == "QWERTY" and ProdutoModel.filial_id == 1).first()
-    produto_destino = db.query(ProdutoModel).filter(
-        ProdutoModel.codigo_barras == "QWERTY" and ProdutoModel.filial_id == 2).first()
+    def test_recebimento_negativo(self):
 
-    produto_origem.quantidade_disponivel -= 5
-    produto_destino.quantidade_disponivel += 5
+        transacao_data = {
+            "quantidade": -5,
+            "codigo_barras": "QWERTY",
+            "fornecedor_id": 1,
+            "filial_id": 1
+        }
 
-    response = client.post("/transacao/transferencia", json=transacao_data)
+        response = client.post("/transacao/recebimento", json=transacao_data)
 
-    produtos = []
-    produtos.append(produto_origem)
-    produtos.append(produto_destino)
+        assert response.status_code == 400
+        assert response.json() == {
+            "detail": "Quantidade da transação não deve ser menor que zero."}
 
-    assert response.status_code == 200
-    assert response.json() == [transacao_view.format_response(
-        produto_model) for produto_model in produtos]
+        self.db.close()
 
-    db.close()
+    def test_transferencia_negativa(self):
+
+        transacao_data = {
+            "quantidade": -5,
+            "codigo_barras": "QWERTY",
+            "filial_origem": 1,
+            "filial_destino": 2
+        }
+
+        response = client.post("/transacao/transferencia", json=transacao_data)
+
+        assert response.status_code == 400
+        assert response.json() == {
+            "detail": "Quantidade da transação não deve ser menor que zero."}
+
+        self.db.close()
+
+    def test_devolucao_negativa(self):
+
+        transacao_data = {
+            "quantidade": -5,
+            "codigo_barras": "QWERTY",
+            "tipo": "devolucao",
+            "filial_id": 1
+        }
+
+        response = client.post("/transacao", json=transacao_data)
+
+        assert response.status_code == 400
+        assert response.json() == {
+            "detail": "Quantidade da transação não deve ser menor que zero."}
+
+        self.db.close()
+
+    def test_venda_negativa(self):
+
+        transacao_data = {
+            "quantidade": -5,
+            "codigo_barras": "QWERTY",
+            "tipo": "venda",
+            "filial_id": 1
+        }
+
+        response = client.post("/transacao", json=transacao_data)
+
+        assert response.status_code == 400
+        assert response.json() == {
+            "detail": "Quantidade da transação não deve ser menor que zero."}
+
+        self.db.close()
