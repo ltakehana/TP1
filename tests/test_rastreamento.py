6aa3ef96e6569c6097c10254c31e719f9f157e5e
PauloAbiAcl <pv.silva23@hotmail.com>
2023-12-03 23:21:49 -0300

    feat: parametrizando testes rastreamento

--- tests/test_rastreamento.py
+++ tests/test_rastreamento.py
@@ -15,46 +15,22 @@ def setup_db():
     db = SessionLocal()
     return db
 
-def test_cadastrar_lote1():
-    db = setup_db()
-    
-    # produto_data = {
-    #     "descricao": "Shampoo", 
-    #     "codigo_barras": "12345",
-    #     "custo": 10.0,
-    #     "preco_venda": 20.0,
-    #     "quantidade_disponivel": 50
-    # }
-    lote_data = {
+@pytest.mark.parametrize("lote_data", [
+    {
         "id": 2121,
         "quantidade": 10,
         "produto_id": 1,
         "data_validade": "2023-12-11T00:44:07.446Z"
-    }
-    
-    response = client.post("/lote/", json=lote_data)
-
-    assert response.status_code == 200
-    assert response.json() == lote_data
-
-    db.close()
-    
-def test_cadastrar_lote2():
-    db = setup_db()
-    
-    # produto_data = {
-    #     "descricao": "Shampoo", 
-    #     "codigo_barras": "12345",
-    #     "custo": 10.0,
-    #     "preco_venda": 20.0,
-    #     "quantidade_disponivel": 50
-    # }
-    lote_data = {
+    },
+    {
         "id": 2020,
         "quantidade": 22,
         "produto_id": 1,
         "data_validade": "2024-05-12T00:44:07.446Z"
     }
+])
+def test_cadastrar_lote(lote_data):
+    db = setup_db()
     
     response = client.post("/lote/", json=lote_data)
 
@@ -62,31 +38,39 @@ def test_cadastrar_lote2():
     assert response.json() == lote_data
 
     db.close()
-     
+
 def test_lote_nao_encontrada():
     response = client.get('lote/' + '10999' + '/')
     
     assert response.status_code == 400
     assert response.json() == {"detail":"Não foi encontrado nenhum seletor com tal id."}
 
-def test_quantidade_produto_lote():   
+@pytest.mark.parametrize("lote_data, expected_quantidade", [
+    ({
+        "id": 2222,
+        "quantidade": 10,
+        "produto_id": 1,
+        "data_validade": "2023-12-11T00:44:07.446Z"
+    }, 10),
+    ({
+        "id": 2223,
+        "quantidade": 100,
+        "produto_id": 1,
+        "data_validade": "2024-08-21T00:44:07.446Z"
+    }, 100),
+])
+def test_quantidade_produto_lote(lote_data, expected_quantidade):   
     db = setup_db()
     
-    lote = LoteSchema(
-        id = 2222,
-        quantidade = 10,
-        produto_id = 1,
-        data_validade = "2023-12-11T00:44:07.446Z"
-    )
-    
+    lote = LoteSchema(**lote_data)
     lote_db = LoteModel(**lote.dict())
     db.add(lote_db)
     db.commit()
     db.refresh(lote_db)
     
-    response = client.get('lote/' + str(lote.id) + '/')
+    response = client.get(f'lote/{lote.id}/')
     
     assert response.status_code == 200
-    assert response.json() == {"quantidade": lote.quantidade}
+    assert response.json() == {"quantidade": expected_quantidade}
     
     db.close()
